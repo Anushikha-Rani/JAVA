15e7fe0730e786caf0d280fab759ccefc14f5b81
Anushikha-Rani <ranianushikha@gmail.com>
2024-05-31 00:08:47 +0530

    loop_dowhile

--- input_cal.java
+++ input_cal.java
@@ -1,6 +1,8 @@
+
 //take 2 variable 'a' & 'b' and print their sum
 import java.util.*;
-class HelloWorld {
+
+class input_cal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
--- multi.java
+++ multi.java
@@ -1,11 +1,11 @@
 //print the multiple of 2 number:
 
-class HelloWorld {
+class multi {
   public static void main(String[] args) {
-      int a = 10;
-      int b = 25;
-      int mul = a * b;
-      System.out.println(mul);
-    
+    int a = 10;
+    int b = 25;
+    int mul = a * b;
+    System.out.println(mul);
+
   }
 }
