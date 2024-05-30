f23dd879fd3e31657727250d88f34c22e50e7abd
Anushikha Rani <150791412+Anushikha-Rani@users.noreply.github.com>
2024-05-30 23:11:00 +0530

    Delete loop_for.java

--- loop_for.java
+++
@@ -1,9 +0,0 @@
-//loops: for loop -> print 0 to 10
-import java.util.*;
-class HelloWorld {
-    public static void main(String[] args) {
-        for(int i =0; i< 11;i++){
-            System.out.print( i );
-        }
-    }
-}
