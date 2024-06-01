14fd575e42a02f1ab0e792a8ad2fe5f446268487
Anushikha-Rani <ranianushikha@gmail.com>
2024-06-01 22:56:14 +0530

    nested_loop7

--- nested_loop7.java
+++ nested_loop7.java
@@ -4,10 +4,10 @@ public class nested_loop7 {
   public static void main(String args[]) {
     int n = 5;
 
-    for(int i=1;i<=n;i++){
-      for(int j=1;j<=i;j++){
-        System.out.print(j+" "+j);
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print(j + " " + j);
       }
     }
   }
-} 
+}
