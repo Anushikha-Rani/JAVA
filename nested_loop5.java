1cbfc50295ea4764f601c47dbe9a8698445ebcdb
Anushikha-Rani <ranianushikha@gmail.com>
2024-06-01 23:08:27 +0530

    nested_loop4

--- nested_loop5.java
+++ nested_loop5.java
@@ -1,10 +1,13 @@
-import java.util.*;
-public static void main(String args[]){
-  int n= 4;
-  for(int i=n; i>=1; i--){
-    for(int j=1;j<=i;j++){
-      System.out.print("*");
+
+publ  
+public static void main(String args[]) {
+
+  int n= 4; 
+   for(int i=n; i>=1; i--){
+      for(int j=1; j<=i;j++) {
+          System.out.print("*");
+      }
+        System.out.println();
     }
-    System.out.println();
   }
 }
