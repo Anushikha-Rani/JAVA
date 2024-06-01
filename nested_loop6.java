76a0aee22656a00d1460c06c6308973b5d66de54
Anushikha-Rani <ranianushikha@gmail.com>
2024-06-01 22:48:07 +0530

    nested_loop9

---
+++ nested_loop6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class nested_loop6 {
+  public static void main(Sting args[]) {
+   int  n = 4;        
+   for(int i=1; i<=n;i++){
+     for(int j=1; j<=n-i;j++){
+        System . out. print("") ;
+      }
+      for(int j=1; j<=i; j++){
+        System.out.print("*");
+      }
+    }
+  }
+}
